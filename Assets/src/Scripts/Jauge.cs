a60e3a5e52be43d35c707e95ad103f1b06a23299
Ibramadi75 <ibrahim75madi@gmail.com>
2024-03-15 11:04:58 +0100

    update: countdown screen now must be attached to the parent gameObject

--- Assets/src/Scripts/Jauge.cs
+++ Assets/src/Scripts/Jauge.cs
@@ -1,19 +1,44 @@
+using System;
 using UnityEngine;
 
 public class Jauge : MonoBehaviour
 {
-    [SerializeField] Countdown countdownScript;
+    private Countdown _countdownScript;
     private float _totalTime;
+
+    private float _xDefaultLocalScale;
+    private float _xDefaultLocalPosition;
 
     void Start()
     {
-        _totalTime = countdownScript.time;
+        _countdownScript = GetComponentInParent<Countdown>();
+
+        if (_countdownScript is null)
+        {
+            Debug.Log("A countdown script must be attached to the parent.");
+        }
+        else
+        {
+            _totalTime = _countdownScript.time;
+            _xDefaultLocalScale = transform.localScale.x;
+            _xDefaultLocalPosition = transform.localPosition.x;
+        }
     }
 
     void Update()
     {
-        float remainingTime = countdownScript.TimeLeft;
-        transform.localScale = new Vector3(remainingTime / _totalTime, transform.localScale.y, transform.localScale.z);
-        transform.localPosition = new Vector3((remainingTime / _totalTime)/2 -.5f, transform.localPosition.y, transform.localPosition.z);
+        float remainingTime = _countdownScript.TimeLeft;
+        Vector3 localScale = transform.localScale;
+        Vector3 localPosition = transform.localPosition;
+
+        float xLocalScale = remainingTime / _totalTime + _xDefaultLocalScale;
+        float xLocalPosition = (remainingTime / _totalTime) / 2 + _xDefaultLocalPosition;
+
+        if (xLocalScale >= 0)
+        {
+            transform.localScale = new Vector3(xLocalScale, localScale.y, localScale.z);
+        }
+        
+        transform.localPosition = new Vector3(xLocalPosition, localPosition.y, localPosition.z);
     }
 }
