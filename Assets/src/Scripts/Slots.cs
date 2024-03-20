e7aabf4474971bd468d08117de16a46deddde8d4
ibrahim <98467166+Ibramadi75@users.noreply.github.com>
2024-03-20 08:45:26 +0100

    feat/slots-script (#5)
    * merge
    
    * feat: Implement flexible storage system with Slots script
    
    - Introduce a storage system allowing any GameObject to store and retrieve other GameObjects.
    - Customizable capacity to control the number of slots available.
    - Implemented functions for storing and retrieving GameObjects.
    - Added option for infinite storage, ensuring a GameObject's slot never remains empty even after retrieval.

---
+++ Assets/src/Scripts/Slots.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slots : MonoBehaviour
+{
+    [SerializeField] private GameObject[] slots;
+    [SerializeField] private uint maxCapacity;
+    [SerializeField] bool isInfinite = false; // Defines if the slots are infinite or not.
+    public uint GetCapacity() => maxCapacity;
+
+    void Start()
+    {
+        if (maxCapacity <= 0 || isInfinite)
+            maxCapacity = 1;
+
+        // Check if the slots array is not initialized from the inspector
+        if (slots == null || slots.Length != maxCapacity)
+        {
+            GameObject[] newSlots = new GameObject[maxCapacity];
+            
+            // Preserve values from the old array if it's initialized
+            if (slots != null)
+            {
+                int copyLength = Mathf.Min(slots.Length, (int)maxCapacity);
+                for (int i = 0; i < copyLength; i++)
+                {
+                    newSlots[i] = slots[i];
+                }
+            }
+
+            slots = newSlots;
+        }
+    }
+
+    public bool Store(GameObject obj)
+    {
+        for (int i = 0; i < maxCapacity; i++)
+        {
+            if (slots[i] == null)
+            {
+                slots[i] = obj;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public GameObject Retrieve()
+    {
+        Debug.Log("Slot Retrieve");
+        for (int i = 0; i < maxCapacity; i++)
+        {
+            if (slots[i] != null)
+            {
+                if (isInfinite)
+                    return slots[i];
+                else
+                {
+                    var obj = slots[i];
+                    slots[i] = null;
+                    return obj;
+                }
+            }
+        }
+
+        return null;
+    }
+}
