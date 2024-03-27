0aece00a81063a2cba40954184af014ddd943b46
Ibramadi75 <ibrahim75madi@gmail.com>
2024-03-27 12:00:47 +0100

    feat/countdown-by-interaction

--- Assets/src/Scripts/Countdown.cs
+++ Assets/src/Scripts/Countdown.cs
@@ -6,6 +6,8 @@ public class Countdown : MonoBehaviour
     public float time = 10f;
     private float _timeLeft;
     private Coroutine countdownCoroutine;
+    [SerializeField] bool byInteraction = false;
+    [SerializeField] bool launchCoroutine = false;
 
     public float TimeLeft
     {
@@ -15,23 +17,35 @@ public class Countdown : MonoBehaviour
     void Start()
     {
         _timeLeft = time;
+
+        if (!byInteraction)
+            StartCoroutine(StartCountdown());
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKey(KeyCode.Space))
+            Debug.Log("AAA -> Countdown time: " + _timeLeft);
+        else
+            Debug.Log("FFF -> Countdown time: " + _timeLeft);
+
+        if (byInteraction)
         {
-            if (countdownCoroutine == null)
+            if (launchCoroutine)
             {
-                countdownCoroutine = StartCoroutine(StartCountdown());
+                if (countdownCoroutine == null)
+                {
+                    countdownCoroutine = StartCoroutine(StartCountdown());
+                }
             }
-        }
-        else if (Input.GetKeyUp(KeyCode.E))
-        {
-            if (countdownCoroutine != null)
+            else
             {
-                StopCoroutine(countdownCoroutine);
-                countdownCoroutine = null;
+                if (countdownCoroutine != null)
+                {
+                    StopCoroutine(countdownCoroutine);
+                    countdownCoroutine = null;
+                    launchCoroutine = false;
+                }
             }
         }
     }
