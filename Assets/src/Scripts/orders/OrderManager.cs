a55bd01d0fb4daaff18e91f35a3d071c0fecfe01
raph <orsi.raph@gmail.com>
2024-03-30 14:47:05 +0100

    feat: order creation and destroy + rearrange positions

---
+++ Assets/src/Scripts/orders/OrderManager.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class OrderManager : MonoBehaviour
+{
+    [SerializeField] private float yOffset = -0.18f;
+    [SerializeField] private GameObject anOrderPrefab;
+
+    private Dictionary<int, Order> _activeOrders;
+    private Transform _screenUi;
+    private int _counter;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _screenUi = transform.GetChild(0);
+        _activeOrders = new Dictionary<int, Order>
+        {
+            [0] = null,
+            [1] = null,
+            [2] = null,
+            [3] = null
+        };
+
+        StartCoroutine(SpawnOrderPeriodically());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AnyOrderExpired())
+        {
+            List<int> expiredOrderKeys = new List<int>();
+            
+            foreach (var activeOrder in _activeOrders)
+            {
+                if (activeOrder.Value.HasExpired())
+                {
+                    Destroy(activeOrder.Value.gameObject);
+                    expiredOrderKeys.Add(activeOrder.Key);
+                }
+            }
+            
+            expiredOrderKeys.ForEach(key => _activeOrders[key] = null);
+            RearrangeOrders();
+        }
+    }
+
+    IEnumerator SpawnOrderPeriodically()
+    {
+        while (true)
+        {
+            int position = FindFirstAvailablePosition();
+            if (position != -1)
+                CreateOrder(FindFirstAvailablePosition());
+            yield return new WaitForSeconds(3f);
+        }
+    }
+    
+    void CreateOrder(int positionIndex)
+    {
+        GameObject order = Instantiate(anOrderPrefab, _screenUi);
+        order.transform.localPosition -= Vector3.down * yOffset * positionIndex;
+        _activeOrders[positionIndex] = order.GetComponent<Order>();
+    }
+
+    void RearrangeOrders()
+    {
+        for (int i = 0; i < _activeOrders.Count; i++)
+        {
+            if (_activeOrders[i] is not null)
+            {
+                int newPosition = i - 1;
+                if (newPosition >= 0)
+                {
+                    _activeOrders[newPosition] = _activeOrders[i];
+                    _activeOrders[newPosition].transform.localPosition -= Vector3.up * yOffset;
+                    _activeOrders[i] = null;
+                }
+            }
+        }
+    }
+    
+    bool AnyOrderExpired() => _activeOrders.Any(activeOrder => activeOrder.Value is not null && activeOrder.Value.HasExpired());
+
+    int FindFirstAvailablePosition()
+    {
+        foreach (var activeOrder in _activeOrders)
+            if (activeOrder.Value is null) return activeOrder.Key;
+        return -1;
+    }
+}
