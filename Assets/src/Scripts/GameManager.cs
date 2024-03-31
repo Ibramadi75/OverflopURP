3c8523f9a8937b8f09ba0d24282e03e55a789abb
raph <orsi.raph@gmail.com>
2024-03-31 22:07:52 +0200

    feat: add main game loop

---
+++ Assets/src/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float _timer;
+    [SerializeField] private TMP_Text _timerText;
+
+    void Update()
+    {
+        UpdateTimer();
+        if (_timer <= 0)
+        {
+            Debug.Log("Loser tu es nul");
+            Time.timeScale = 0;
+            _timerText.gameObject.SetActive(false);
+        }
+    }
+
+    void UpdateTimer()
+    {
+        _timer -= Time.deltaTime;
+        _timerText.text = $"Temps restant: {_timer:F2}";
+    }
+}
