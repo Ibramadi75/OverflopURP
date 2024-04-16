d58e766f188d91ba00814f6a6f77c6043b7a80f3
raph <orsi.raph@gmail.com>
2024-04-16 22:08:38 +0200

    feat: begin to implement shop interaction

---
+++ Assets/src/Scripts/interactions/ShopInteraction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ShopInteraction : AbstractInteraction
+{
+    public override void MainInteraction(GameObject author)
+    {
+        Debug.Log("tkt");
+    }
+
+    public override void SecondaryInteraction(GameObject author) { }
+}
