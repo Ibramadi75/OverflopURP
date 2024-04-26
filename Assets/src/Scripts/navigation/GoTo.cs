3615ab6296ae9c2bd985004cfd37fce90dc0359e
raph <orsi.raph@gmail.com>
2024-04-26 21:25:59 +0200

    feat: poc for scripted navigation

---
+++ Assets/src/Scripts/navigation/GoTo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class GoTo : MonoBehaviour
+{
+    [SerializeField] private List<Transform> checkpoints;
+
+    private int _currentCheckpoint = -1;
+    private bool _isMoving;
+    
+    void Update()
+    {
+        if (!_isMoving && _currentCheckpoint < checkpoints.Count - 1)
+        {
+            _currentCheckpoint++;
+            ReachNextCheckpoint();
+        }
+    }
+
+    private void ReachNextCheckpoint()
+    {
+        _isMoving = true;
+        StartCoroutine(ReachCheckpoint(checkpoints[_currentCheckpoint]));
+    }
+
+    private IEnumerator ReachCheckpoint(Transform checkpoint)
+    {
+        transform.DOMove(checkpoint.position, 5f).SetEase(Ease.Linear);
+        yield return new WaitForSeconds(5f);
+        _isMoving = false;
+    }
+}
