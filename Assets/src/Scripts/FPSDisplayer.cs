a868ea3b949d22c6439a1b578f56bf303f1217ad
raph <orsi.raph@gmail.com>
2024-05-02 21:37:04 +0200

    chore: add fps

---
+++ Assets/src/Scripts/FPSDisplayer.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class FPSDisplayer : MonoBehaviour
+{
+    float fps;
+    float updateTimer = .2f;
+    [SerializeField] TextMeshProUGUI fpsTitle;
+
+    void UpdateFPSDisplay()
+    {
+        updateTimer -= Time.deltaTime;
+        if (updateTimer < 0)
+        {
+            fps = 1f / Time.unscaledDeltaTime;
+            fpsTitle.text = "FPS : " + Mathf.Round(fps);
+            updateTimer = .2f;
+        }
+    }
+
+    void Update()
+    {
+        UpdateFPSDisplay();
+    }
+}
